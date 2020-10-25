f0dbbf4ef3413b583e67c5349cfe66239eadf1bf
jonataslaet <jonatas@infoway-pi.com.br>
2020-10-25 01:08:50 -0300

    Implementado o model Produto ajustes com esse model nas requisições GET, PUT e POST

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Produto } from './model/produto.model';
 
 @Component({
   selector: 'app-root',
@@ -15,13 +16,13 @@ export class AppComponent {
   }
 
   listarTodosProdutos(){
-    this.http.get(`${this.apiUrl}/produtos`).subscribe (
+    this.http.get<Produto[]>(`${this.apiUrl}/produtos`).subscribe (
       resultado => console.log(resultado)
     );
   }
 
   listarProdutoPorId(){
-    this.http.get(`${this.apiUrl}/produtos/4`).subscribe(
+    this.http.get<Produto>(`${this.apiUrl}/produtos/4`).subscribe(
       resultado => {
         console.log(resultado);
       },
@@ -34,9 +35,11 @@ export class AppComponent {
   }
 
   adicionarProduto(){
-    var produto = { nome : "" };
+    var produto = new Produto();
+    produto.id = 4;
+    produto.nome = "TV 52 Polegados";
 
-    this.http.post(`${this.apiUrl}/produtos`,produto).subscribe(
+    this.http.post<Produto>(`${this.apiUrl}/produtos`,produto).subscribe(
       resultado => {
         console.log(resultado);
       },
@@ -49,7 +52,9 @@ export class AppComponent {
   }
 
   alterarProduto(){
-    var produto = { id: 1, nome : "TV 52 Polegadas"};
+    var produto = new produto();
+    produto.id = 1;
+    produto.nome = "TV 53 Polegados";
 
     this.http.put(`${this.apiUrl}/produtos/1`, produto).subscribe (
       resultado => {
